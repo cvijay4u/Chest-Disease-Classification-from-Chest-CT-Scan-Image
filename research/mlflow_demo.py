6cb68b59bb2160751fe196b035f66709c021bc0a
cvijay4u <cvijay4u@gmail.com>
2024-03-13 18:07:35 +0530

    MLflow updated

--- research/mlflow_demo.py
+++ research/mlflow_demo.py
@@ -71,6 +71,11 @@ if __name__ == "__main__":
         mlflow.log_metric("mae", mae)
 
 
+        # For remote server only (Dagshub)
+        remote_server_uri = "https://dagshub.com/cvijay4u/ML-flow-Expriement-Demo.mlflow"
+        mlflow.set_tracking_uri(remote_server_uri)
+
+
 
         tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
 
